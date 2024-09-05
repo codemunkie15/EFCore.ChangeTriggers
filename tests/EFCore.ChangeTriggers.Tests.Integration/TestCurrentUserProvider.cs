1520f49995bed6c6780ad1a05ace62720203ee33
Callum Ellis <callum.ellis@hotmail.co.uk>
2024-09-06 00:13:38 +0100

    Started integration tests

---
+++ tests/EFCore.ChangeTriggers.Tests.Integration/TestCurrentUserProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace EFCore.ChangeTriggers.SqlServer.Tests.Integration
+{
+    internal class TestCurrentUserProvider
+    {
+        private readonly TestUser currentUser;
+
+        public TestCurrentUserProvider(TestUser currentUser)
+        {
+            this.currentUser = currentUser;
+        }
+
+        public TestUser GetCurrentUser() => currentUser;
+    }
+}
---
+++ tests/EFCore.ChangeTriggers.Tests.Integration/TestUser.cs
@@ -0,0 +1,13 @@
+﻿using EFCore.ChangeTriggers.Abstractions;
+
+namespace EFCore.ChangeTriggers.SqlServer.Tests.Integration
+{
+    internal class TestUser : ITracked<TestUserChange>
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public ICollection<TestUserChange> Changes { get; set; }
+    }
+}
