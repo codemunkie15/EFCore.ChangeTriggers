b15cd9f9abe9962dd73a91eed86452f6a464490b
Callum Ellis <callum.ellis@hotmail.co.uk>
2025-06-19 22:14:06 +0100

    ChangeEventQueries integration tests

--- tests/EFCore.ChangeTriggers.ChangeEventQueries.Tests.Integration/Tests/ToChangeEventsTests.cs
+++ tests/EFCore.ChangeTriggers.ChangeEventQueries.Tests.Integration/Tests/ToChangeEventsTests.cs
@@ -1,121 +1,119 @@
-﻿//using EFCore.ChangeTriggers.ChangeEventQueries.Configuration;
-//using EFCore.ChangeTriggers.ChangeEventQueries.Exceptions;
-//using EFCore.ChangeTriggers.Tests.Integration.Common.Helpers;
-//using FluentAssertions;
+﻿using EFCore.ChangeTriggers.ChangeEventQueries.Configuration;
+using EFCore.ChangeTriggers.ChangeEventQueries.Exceptions;
+using EFCore.ChangeTriggers.Tests.Integration.Common.Domain;
+using EFCore.ChangeTriggers.Tests.Integration.Common.Persistence;
+using EFCore.ChangeTriggers.Tests.Integration.Common.Tests;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
-//namespace EFCore.ChangeTriggers.ChangeEventQueries.Tests.Integration.Tests
-//{
-//    public class ToChangeEventsTests : IClassFixture<ToChangeEventsTestFixture>
-//    {
-//        private readonly ToChangeEventsTestFixture fixture;
-//        private readonly TestHelper testHelper;
+namespace EFCore.ChangeTriggers.ChangeEventQueries.Tests.Integration.Tests
+{
+    public class ToChangeEventsTests : TestBase<TestDbContext>, IClassFixture<ToChangeEventsTestFixture>
+    {
 
-//        public ToChangeEventsTests(ToChangeEventsTestFixture fixture)
-//        {
-//            this.fixture = fixture;
-//            testHelper = new(fixture.Services);
-//        }
+        public ToChangeEventsTests(ToChangeEventsTestFixture fixture) : base(fixture.Services)
+        {
+        }
 
-//        //[Fact]
-//        //public async Task ToChangeEvents_WithSimpleProperties_ReturnsCorrectChangeEvents()
-//        //{
-//        //    testHelper.CurrentUserProvider.CurrentUserAsync = ChangedByEntityUser.SystemUser;
+        [Fact]
+        public async Task ToChangeEvents_WithSimpleProperties_ReturnsCorrectChangeEvents()
+        {
+            var oldUser = new User
+            {
+                Username = "Test User",
+                IsAdmin = true,
+                LastUpdatedAt = DateTimeOffset.UtcNow
+            };
 
-//        //    var oldUser = new User
-//        //    {
-//        //        Username = "Test User",
-//        //        IsAdmin = true,
-//        //        LastUpdatedAt = DateTimeOffset.UtcNow
-//        //    };
+            var newUser = new User
+            {
+                Username = "Test User Name Change",
+                IsAdmin = false,
+                LastUpdatedAt = DateTimeOffset.UtcNow.AddHours(1)
+            };
 
-//        //    var newUser = new User
-//        //    {
-//        //        Username = "Test User Name Change",
-//        //        IsAdmin = false,
-//        //        LastUpdatedAt = DateTimeOffset.UtcNow.AddHours(1)
-//        //    };
+            var user = new User
+            {
+                Username = oldUser.Username,
+                IsAdmin = oldUser.IsAdmin,
+                LastUpdatedAt = oldUser.LastUpdatedAt
+            };
 
-//        //    var user = new User
-//        //    {
-//        //        Username = oldUser.Username,
-//        //        IsAdmin = oldUser.IsAdmin,
-//        //        LastUpdatedAt = oldUser.LastUpdatedAt
-//        //    };
+            dbContext.TestUsers.Add(user);
+            await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
 
-//        //    testHelper.DbContext.TestUsers.Add(user);
-//        //    await testHelper.DbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+            user.Username = newUser.Username;
+            user.IsAdmin = newUser.IsAdmin;
 
-//        //    user.Username = newUser.Username;
-//        //    user.IsAdmin = newUser.IsAdmin;
+            await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
 
-//        //    await testHelper.DbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+            user.LastUpdatedAt = newUser.LastUpdatedAt;
 
-//        //    user.LastUpdatedAt = newUser.LastUpdatedAt;
+            await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
 
-//        //    await testHelper.DbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+            var changeEvents = await dbContext.TestUserChanges
+                .Where(uc => uc.Id == user.Id)
+                .ToChangeEvents(new ChangeEventConfiguration(builder =>
+                {
+                    builder.Configure<UserChange>(uc =>
+                    {
+                        uc.AddProperty(uc => uc.Username);
+                        uc.AddProperty(uc => uc.IsAdmin.ToString());
+                        uc.AddProperty(uc => uc.LastUpdatedAt.ToString());
+                    });
+                }))
+                .ToListAsync(TestContext.Current.CancellationToken);
 
-//        //    var changeEvents = await testHelper.DbContext.TestUserChanges
-//        //        .Where(uc => uc.Id == user.Id)
-//        //        .ToChangeEvents(new ChangeEventConfiguration(builder =>
-//        //        {
-//        //            builder.Configure<UserChange>(uc =>
-//        //            {
-//        //                uc.AddProperty(uc => uc.Username);
-//        //                uc.AddProperty(uc => uc.IsAdmin.ToString());
-//        //                uc.AddProperty(uc => uc.LastUpdatedAt.ToString());
-//        //            });
-//        //        }))
-//        //        .ToListAsync(TestContext.Current.CancellationToken);
+            changeEvents.Should().BeEquivalentTo(
+            [
+                new ChangeEvent
+                {
+                    Description = $"{nameof(User.Username)} changed",
+                    OldValue = oldUser.Username,
+                    NewValue = newUser.Username,
 
-//        //    changeEvents.Should().BeEquivalentTo(
-//        //    [
-//        //        new ChangeEvent
-//        //        { 
-//        //            Description = $"{nameof(User.Username)} changed",
-//        //            OldValue = oldUser.Username,
-//        //            NewValue = newUser.Username,
+                },
+                new ChangeEvent
+                {
+                    Description = $"{nameof(User.IsAdmin)} changed",
+                    OldValue = oldUser.IsAdmin.ToString(),
+                    NewValue = newUser.IsAdmin.ToString(),
+                },
+                new ChangeEvent
+                {
+                    Description = $"{nameof(User.LastUpdatedAt)} changed",
+                    OldValue = oldUser.LastUpdatedAt.ToString("yyyy-MM-dd HH:mm:ss.fffffff zzz"), // SQL Server format
+                    NewValue = newUser.LastUpdatedAt.ToString("yyyy-MM-dd HH:mm:ss.fffffff zzz"), // SQL Server format
+                }
+            ], options => options.Excluding(ce => ce.ChangedAt));
+        }
 
-//        //        },
-//        //        new ChangeEvent
-//        //        {
-//        //            Description = $"{nameof(User.IsAdmin)} changed",
-//        //            OldValue = oldUser.IsAdmin.ToString(),
-//        //            NewValue = newUser.IsAdmin.ToString(),
-//        //        },
-//        //        new ChangeEvent
-//        //        {
-//        //            Description = $"{nameof(User.LastUpdatedAt)} changed",
-//        //            OldValue = oldUser.LastUpdatedAt.ToString("yyyy-MM-dd HH:mm:ss.fffffff zzz"), // SQL Server format
-//        //            NewValue = newUser.LastUpdatedAt.ToString("yyyy-MM-dd HH:mm:ss.fffffff zzz"), // SQL Server format
-//        //        }
-//        //    ], options => options.Excluding(ce => ce.ChangedAt));
-//        //}
+        [Fact]
+        public void ToChangeEvents_WithNoConfiguration_ThrowsException()
+        {
+            var changeEvents = () => dbContext.TestUserChanges.ToChangeEvents();
 
-//        [Fact]
-//        // Test exceptions
-//        public void ToChangeEvents_WithNoConfiguration_ThrowsException()
-//        {
-//            var changeEvents = () => testHelper.DbContext.TestUserChanges.ToChangeEvents();
+            changeEvents.Should()
+                .Throw<ChangeEventQueryException>()
+                .WithMessage("No configuration found.*");
+        }
 
-//            changeEvents.Should()
-//                .Throw<ChangeEventQueryException>()
-//                .WithMessage("No configuration found.*");
-//        }
+        [Fact]
+        public void ToChangeEvents_WithNoConfigurationForEntity_ThrowsException()
+        {
+            var changeEvents = () => dbContext.TestUserChanges.ToChangeEvents(new ChangeEventConfiguration());
 
-//        [Fact]
-//        public void ToChangeEvents_WithNoConfigurationForEntity_ThrowsException()
-//        {
-//            var changeEvents = () => testHelper.DbContext.TestUserChanges.ToChangeEvents(new ChangeEventConfiguration());
+            changeEvents.Should()
+                .Throw<ChangeEventQueryException>()
+                .WithMessage("No configuration found for entity type UserChange.");
+        }
 
-//            changeEvents.Should()
-//                .Throw<ChangeEventQueryException>()
-//                .WithMessage("No configuration found for entity type UserChange.");
-//        }
+        [Fact]
+        public void ToChangeEvents_WithDbContextConfiguration_ReturnsCorrectChangeEvents()
+        {
+            
+        }
 
-//        // Test passed in config
-
-//        // Test DbContext config
-
-//        // Test include inserts/deletes
-//    }
-//}
+        // Test include inserts/deletes
+    }
+}
